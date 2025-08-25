9035c96cf1789a1fc92b3464f2ac5161a208e6bd
afifhikam <apipidnschool@gmail.com>
2025-08-26 01:25:28 +0700

    first commit

--- pertemuan-19/latihan19SOAL1.py
+++ pertemuan-19/latihan19SOAL1.py
@@ -1,12 +1,6 @@
-#SOAL 1
-
 import re
 
 data = "Pendapatan bulan ini adalah Rp 1,500,000, sedangkan pengeluaran sebesar Rp 850,000."
-
-pattern = r"\d+"
-
-hasil = re.findall(pattern, data)
+hasil = re.findall(r"\d+", data)
 
 print(hasil)
-
